a212a0e2d54a720ea2b5ec8ab13c75ec83170be6
esposito.00@outlook.it <esposito.00@outlook.it>
2023-01-25 18:07:36 +0100

    html css

--- index.php
+++ index.php
@@ -1,49 +1,75 @@
-<!-- immaginare quali sono le classi necessarie per creare uno shop online con le seguenti caratteristiche:
+<!DOCTYPE html>
+<html lang="en">
 
-- L'e-commerce vende **prodotti** per animali
-- I prodotti sono categorizzati, le **categorie** sono Cani o Gatti
-- I prodotti saranno suddivisi anche per **tipologia** come cibo, giochi, cucce, etc
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 
-Stampiamo delle card contenenti i dettagli dei prodotti -->
+<body>
+    <header>
+        <h1>Animals Product</h1>
+    </header>
 
-<?php
-class AnimalsProducts
-{
-    // variabili
-    public $name;
-    public $category;
-    public $typology;
-    public $price;
+    <main>
+        <?php
+        class AnimalsProducts
+        {
+            // variabili
+            public $name;
+            public $category;
+            public $typology;
+            public $price;
 
-    // costruttore
-    public function __construct($_name, $_category, $_typology, $_price)
-    {
-        $this->name = $_name;
-        $this->category = $_category;
-        $this->typology = $_typology;
-        $this->price = $_price;
-    }
+            // costruttore
+            public function __construct($_name, $_category, $_typology, $_price)
+            {
+                $this->name = $_name;
+                $this->category = $_category;
+                $this->typology = $_typology;
+                $this->price = $_price;
+            }
 
-    // metodo
-    public function getHtml()
-    {
-        return
-            '<h3>' . $this->name . '</h3>'
-            . '<ul>
+            // metodo
+            public function getHtml()
+            {
+                return
+                    '<h3>' . $this->name . '</h3>'
+                    . '<ul>
                 <li>Category: ' . $this->category . '</li>'
-            . '<li>Typology: ' . $this->typology . '</li>'
-            . '<li>Price: ' . $this->price . '</li>
+                    . '<li>Typology: ' . $this->typology . '</li>'
+                    . '<li>Price: ' . $this->price . ' €</li>
              </ul>';
+            }
+        }
+
+        // oggetti/istanze
+        $products = [
+            $leash = new AnimalsProducts('Leash', 'dogs', 'accessories', 15),
+            $bone = new AnimalsProducts('Bone', 'dogs', 'toys', 5),
+            $ball = new AnimalsProducts('Ball', 'cats', 'toys', 3),
+        ];
+
+        foreach ($products as $product) {
+            echo $product->getHtml();
+        }
+        ?>
+    </main>
+</body>
+
+</html>
+
+
+<style>
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
     }
-}
 
-// oggetti/istanze
-$products = [
-    $leash = new AnimalsProducts('Leash', 'dogs', 'accessories', 15),
-    $bone = new AnimalsProducts('Bone', 'dogs', 'toys', 5),
-    $ball = new AnimalsProducts('Ball', 'cats', 'toys', 3),
-];
-
-foreach ($products as $product) {
-    echo $product->getHtml();
-}
+    main {
+        margin: 40px;
+    }
+</style>
